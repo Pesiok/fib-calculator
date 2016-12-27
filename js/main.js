077bfea87689f20b6d70fd3a8f0eaed16e53657f
Grzegorz Bielski <pesiok@gmail.com>
2016-12-27 01:04:14 +0100

    Another refactor
    Added IIFE and removed event handlers from main Init.

--- js/main.js
+++ js/main.js
@@ -1,65 +1,77 @@
-///// UI
-function labelHolder() {
-    var input = document.querySelector(".calc-div .input-group input");
-  
-    input.addEventListener("blur", function(event){
-    if(this.value === "") {
-      this.classList.remove("has-value");
-    } else {
-      this.classList.add("has-value");
-    }
-  });
-}
-function fib(n) {  
-    //for every interation fibNr = pre + pre2
-     function fibMemo(i, pre, pre2) {
-         //fibMemo(fib number index from 0, 
-         // fib number for previous index, 
-         // fib numer for pre-previous index)
-            return (i == n) ? (pre + pre2) : fibMemo(i+1, pre + pre2, pre);
+(function(){
+    ///// UI /////
+    
+    function labelHolder() {
+        var input = document.querySelector(".calc-div .input-group input");
+
+        input.addEventListener("blur", function(event){
+        if(this.value === "") {
+          this.classList.remove("has-value");
+        } else {
+          this.classList.add("has-value");
         }
-    return (n == 1 | n == 2) ? 1 : fibMemo(3, 1, 1); 
-}
-function parseInput(n) {
-    return (isNaN(n) === true || n < 1 || n > 1476 ) ? false : parseInt(n, 10);
-}
-function displayResults(nr, prevNr, input) {
-    var outputTag = document.getElementById("result");
-    
-    if (nr === undefined) {
-        outputTag.innerHTML = "<span class='result'>Incorrect value.</span><br> Type in number from 1 to 1476.";
-    } else {
-        outputTag.innerHTML = "Fibonacci number of index " + input.value + 
-            " is: <br><br><span class='result'> " + nr + "</span><br><br>" + 
-            "Approximation of &Phi; is: <br><br><span class='result'> " + 
-            (nr/prevNr) + "</span>"; 
+      });
     }
-}
-function fibCalculator() {
-    var input = document.getElementById('input');
-    var outcome = parseInput(input.value);
-
-    if(outcome == false) {
-        displayResults();
-    } else {
-        var number = fib(outcome);
-        var prevNumber = (outcome == 1) ? 1 : fib(outcome - 1);
-
-        displayResults(number, prevNumber, input);
-    }    
-}
-///// Init
-var init = function() {
-    var button = document.getElementById("generate");
-    var input = document.getElementById("input")
+    ///// Calculator /////
     
-    button.addEventListener("click", fibCalculator);
-    input.addEventListener("keyup", function(e){
-        (e.keyCode === 13) ? fibCalculator() : e.preventDefault(); 
+    function fib(n) {  
+        //for every interation fibNr = pre + pre2
+         function fibMemo(i, pre, pre2) {
+             //fibMemo(fib number index from 0, 
+             // fib number for previous index, 
+             // fib numer for pre-previous index)
+                return (i == n) ? (pre + pre2) : fibMemo(i+1, pre + pre2, pre);
+            }
+        return (n == 1 | n == 2) ? 1 : fibMemo(3, 1, 1); 
+    }
+    function parseInput(n) {
+        return (isNaN(n) === true || n < 1 || n > 1476 ) ? false : parseInt(n, 10);
+    }
+    function displayResults(nr, prevNr, input) {
+        var outputTag = document.getElementById("result");
+
+        if (nr === undefined) {
+            outputTag.innerHTML = "<span class='result'>Incorrect value.</span><br> Type in number from 1 to 1476.";
+        } else {
+            outputTag.innerHTML = "Fibonacci number of index " + input.value + 
+                " is: <br><br><span class='result'> " + nr + "</span><br><br>" + 
+                "Approximation of &Phi; is: <br><br><span class='result'> " + 
+                (nr/prevNr) + "</span>"; 
+        }
+    }
+    function fibCalcInit() {
+        var input = document.getElementById('input');
+        var button = document.getElementById("generate");
+
+        function fibCalc() {
+            //Input validation
+            var outcome = parseInput(input.value);
+
+            if(outcome == false) {
+                //In case of inappropriate input
+                displayResults();
+            } else {
+                //Proper input
+                var number = fib(outcome);
+                var prevNumber = (outcome == 1) ? 1 : fib(outcome - 1);
+                
+                displayResults(number, prevNumber, input);
+            }    
+        }
+        
+        button.addEventListener("click", fibCalc);
+        input.addEventListener("keyup", function(e){
+            (e.keyCode === 13) ? fibCalc() : e.preventDefault(); 
+        });
+    }
+    ///// Init /////
+    
+    window.addEventListener("load", function() {
+        // Calculator initializer
+        fibCalcInit();
+        // Holds the label
+        labelHolder();
     });
-    
-    labelHolder();
-}
+})();
 
-window.addEventListener("load", init);
     
